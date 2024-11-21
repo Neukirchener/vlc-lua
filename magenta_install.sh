3081ca78686797b9e00ee21740e6e148a40b5538
Neukirchener <131665749+Neukirchener@users.noreply.github.com>
2024-11-21 13:49:16 +0100

    Update magenta_install.sh

--- magenta_install.sh
+++ magenta_install.sh
@@ -3,7 +3,7 @@
 # Variablen definieren
 VLC_EXT_DIR="$HOME/.local/share/vlc/lua/extensions"
 SCRIPT_NAME="magenta.lua"
-REPO_URL="https://github.com/Neukirchener/vlc-lua/blob/main/$SCRIPT_NAME"
+REPO_URL="https://raw.githubusercontent.com/Neukirchener/vlc-lua/refs/heads/main/$SCRIPT_NAME"
 
 # Verzeichnis erstellen, falls nicht vorhanden
 mkdir -p "$VLC_EXT_DIR"
