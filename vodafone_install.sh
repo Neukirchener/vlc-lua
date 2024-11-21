de31eb2dd329f3168adadf5c7fd6e3fc58f51cae
Neukirchener <131665749+Neukirchener@users.noreply.github.com>
2024-11-21 19:45:50 +0100

    Update vodafone_install.sh

--- vodafone_install.sh
+++ vodafone_install.sh
@@ -3,7 +3,7 @@
 # Variablen definieren
 VLC_EXT_DIR="$HOME/.local/share/vlc/lua/extensions"
 SCRIPT_NAME="vodafone.lua"
-REPO_URL="https://github.com/Neukirchener/vlc-lua/blob/70355e25e60328e4b5883e52b7a02a74aab36aad/lua/$SCRIPT_NAME"
+REPO_URL="https://github.com/Neukirchener/vlc-lua/blob/b2b9366311142a3a3a593afd79460f82d4ed0081/lua/vodafone.lua/$SCRIPT_NAME"
 
 # Verzeichnis erstellen, falls nicht vorhanden
 mkdir -p "$VLC_EXT_DIR"
